b10c578da5023e8704d94e7f9107f463fb240d5a
Thomas Marek <ttmarek@outlook.com>
2016-12-30 20:02:11 -0500

    Delete unneeded files

--- requests.js
+++
@@ -1,8 +0,0 @@
-const { axios } = require('axios');
-
-const getNewDeck = count =>
-  axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${count}`);
-
-module.exports = {
-  getNewDeck,
-};
